a4c03c4ced3c4c607762ae102d390eb56e4b1a36
Curvu <curvu6@gmail.com>
2025-04-29 11:32:47 +0100

    private and public routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,30 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate, Outlet } from 'react-router-dom';
 import React from 'react';
 import Home from 'pages/Home';
 import Register from 'pages/Register';
 import Login from 'pages/Login';
 
+const isLoggedIn = () => !!localStorage.getItem('token');
+
+const PrivateRoutes = () => {
+  return isLoggedIn() ? <Outlet /> : <Navigate to='/login' replace />;
+};
+
+const PublicRoutes = () => {
+  return isLoggedIn() ? <Navigate to='/' replace /> : <Outlet />;
+};
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path='/' element={<Home />} />
+        </Route>
+        <Route element={<PublicRoutes />}>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
