091479025bf2f9cc8e30f43bbbd27bd9e63a2838
Curvu <curvu6@gmail.com>
2025-05-09 21:10:02 +0100

    dockering database

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -34,12 +34,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.messages',
+    # 'django.contrib.staticfiles',
     'api',
     'corsheaders',
     'rest_framework',
@@ -85,12 +85,15 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
-    }
+        # 'NAME': os.getenv('DB_NAME'),
+        # 'PASSWORD': os.getenv('DB_PASSWORD'),
+        # 'HOST': os.getenv('DB_HOST'),
+        'NAME': 'database',
+        'PASSWORD': 'postgres',
+        'HOST': 'database',
+    },
 }
 
 
